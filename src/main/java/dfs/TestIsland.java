7e1af3eb8b728ed607242e4cfee2c2132bb972fb
elchin.abdullayev <elchin.abdullayev@accenture.com>
2019-02-05 17:27:17 +0200

    counting islands with recursiv dfs.

---
+++ src/main/java/dfs/TestIsland.java
@@ -0,0 +1,17 @@
+package bfs;
+
+public class TestIsland {
+    public static void main(String[] args) {
+        int[][] array = {
+                {1,1,1,0,0,0},
+                {1,1,0,0,0,0},
+                {0,0,0,1,1,1},
+                {0,0,0,0,0,0},
+                {0,1,1,0,0,1},
+                {0,1,1,0,0,1},
+                {0,0,0,1,0,1}
+        };
+
+        System.out.println(new Island().findIlands(array));
+    }
+}
---
+++ src/main/java/dfs/recursion/Island.java
@@ -0,0 +1,4 @@
+package dfs.recursion;
+
+public class Island {
+}
